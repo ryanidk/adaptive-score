5ab18ec4f8d85f23e8790fa5f81425ea58fc5d43
Ryan <46634687+ryanidk@users.noreply.github.com>
2026-04-22 09:05:56 -0400

    Add some comments

--- scripts/question_scraping/listskills.py
+++ scripts/question_scraping/listskills.py
@@ -7,12 +7,17 @@ Apr 20, 2026
 """
 
 import json
+
+# Set up a set of skills. The purpose of a set is that avoids duplicate elements.
 skills = set()
 
+# Open up the question bank
 with open("english.json", "r") as f:
     questions = json.load(f)
 
+# Loop over the questions and adds the skill category.
 for question in questions:
     skills.add(question["skill_cd"])
 
+# Print the skills
 print(skills)
