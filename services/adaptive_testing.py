360203557b314e17999eef60a8b93b61ca8527bb
Ryan <46634687+ryanidk@users.noreply.github.com>
2026-04-29 08:31:01 -0400

    add a method to fetch individual skills

---
+++ services/adaptive_testing.py
@@ -0,0 +1,9 @@
+"""
+Adaptive Testing Engine
+ICS3U-01
+Ryan
+This program contains functions for adaptive testing, such as getting a random question based on any random skill.
+It also handles question submission.
+Last modified: Apr 29, 2026
+"""
+
